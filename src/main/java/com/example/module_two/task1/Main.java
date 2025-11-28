9edaaf83b85b24fd5d86cdff5b372d4d9db8d967
alsu-ismayilzade <alsu.ismayilzade@aist.group>
2025-11-28 19:29:36 +0400

    adding configuration and jar file

---
+++ src/main/java/com/example/module_two/task1/Main.java
@@ -0,0 +1,7 @@
+package com.example.module_two.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My simple application is running vith jar file");
+    }
+}
